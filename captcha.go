1b64f1616d6a97af4842853ba45bbe32089b4439
nantawut.thk <nantawut.tantithanakornkul@ktb.co.th>
2018-12-14 01:15:17 +0700

    Create new file captcha.go & captcha_test.go and test passed by function GivePattern1LeftOperand1Operator1RightOperand1_Returns1PlusOne

---
+++ captcha.go
@@ -0,0 +1,5 @@
+package captcha
+
+func Captcha(pattern int, leftOperand int, operator int, rightOperand int) string {
+	return "1 + One"
+}
---
+++ captcha_test.go
@@ -0,0 +1,18 @@
+package captcha
+
+import "testing"
+
+func TestCaptcha_GivePattern1LeftOperand1Operator1RightOperand1_Returns1PlusOne(t *testing.T) {
+	pattern := 1
+	leftOperand := 1
+	operator := 1
+	rightOperand := 1
+
+	expected := "1 + One"
+
+	actual := Captcha(pattern, leftOperand, operator, rightOperand)
+
+	if expected != actual {
+		t.Errorf("It should be %q but get to %q", expected, actual)
+	}
+}
