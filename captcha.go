e2c27c586364d25f58c65a29f11f2f926446c840
nantawut.thk <nantawut.tantithanakornkul@ktb.co.th>
2018-12-17 02:17:31 +0700

    Refactor captcha.go by make slice to store the character of numbers 'One to Nine' only for represent

--- captcha.go
+++ captcha.go
@@ -1,16 +1,11 @@
 package captcha
 
 func Captcha(pattern int, leftOperand int, operator int, rightOperand int) string {
-	if pattern == 1 && leftOperand == 1 && operator == 1 && rightOperand == 1 {
-		return "1 + One"
-	}
 
-	if pattern == 1 && leftOperand == 1 && operator == 1 && rightOperand == 2 {
-		return "1 + Two"
-	}
+	numWords := [9]string{"One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"}
 
-	if pattern == 1 && leftOperand == 1 && operator == 1 && rightOperand == 3 {
-		return "1 + Three"
+	if pattern == 1 && leftOperand == 1 && operator == 1 {
+		return "1 + " + numWords[rightOperand-1]
 	}
 
 	return ""
