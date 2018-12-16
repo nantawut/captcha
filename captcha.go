007562773aaf635fef9163013df1d607cc6c132b
nantawut.thk <nantawut.tantithanakornkul@ktb.co.th>
2018-12-17 01:34:17 +0700

    Create new file captcha.go & captcha_test.go and test passed by function GivePattern1LeftOperand1Operator1RightOperand2_Returns1PlusTwo

--- captcha.go
+++ captcha.go
@@ -1,5 +1,13 @@
 package captcha
 
 func Captcha(pattern int, leftOperand int, operator int, rightOperand int) string {
-	return "1 + One"
+	if pattern == 1 && leftOperand == 1 && operator == 1 && rightOperand == 1 {
+		return "1 + One"
+	}
+
+	if pattern == 1 && leftOperand == 1 && operator == 1 && rightOperand == 2 {
+		return "1 + Two"
+	}
+
+	return ""
 }
--- captcha_test.go
+++ captcha_test.go
@@ -16,3 +16,18 @@ func TestCaptcha_GivePattern1LeftOperand1Operator1RightOperand1_Returns1PlusOne(
 		t.Errorf("It should be %q but get to %q", expected, actual)
 	}
 }
+
+func TestCaptcha_GivePattern1LeftOperand1Operator1RightOperand2_Retruns1PlusTwo(t *testing.T) {
+	pattern := 1
+	leftOperand := 1
+	operator := 1
+	rightOperand := 2
+
+	expected := "1 + Two"
+
+	actual := Captcha(pattern, leftOperand, operator, rightOperand)
+
+	if expected != actual {
+		t.Errorf("It should be %q but get to %q", expected, actual)
+	}
+}
