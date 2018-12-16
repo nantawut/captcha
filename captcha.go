2498dbb836e8f6c249cf6405d2a86e7aa3fa94bb
nantawut.thk <nantawut.tantithanakornkul@ktb.co.th>
2018-12-17 02:47:23 +0700

    Refector captcha.go, make slice to store the character of operator '+, -, x' for represent

--- captcha.go
+++ captcha.go
@@ -5,13 +5,10 @@ import "strconv"
 func Captcha(pattern int, leftOperand int, operator int, rightOperand int) string {
 
 	numWords := [9]string{"One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"}
+	operators := [3]string{" + ", " - ", " x "}
 
-	if pattern == 1 && operator == 1 {
-		return strconv.Itoa(leftOperand) + " + " + numWords[rightOperand-1]
-	}
-
-	if pattern == 1 && operator == 2 {
-		return strconv.Itoa(leftOperand) + " - " + numWords[rightOperand-1]
+	if pattern == 1 {
+		return strconv.Itoa(leftOperand) + operators[operator-1] + numWords[rightOperand-1]
 	}
 
 	return ""
