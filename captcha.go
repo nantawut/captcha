a54a9d0368bd610afc000060e3b72843155febbb
nantawut.thk <nantawut.tantithanakornkul@ktb.co.th>
2018-12-17 02:34:07 +0700

    Refactor captcah.go by Convert leftOperand to string and concat at line return result

--- captcha.go
+++ captcha.go
@@ -1,11 +1,13 @@
 package captcha
 
+import "strconv"
+
 func Captcha(pattern int, leftOperand int, operator int, rightOperand int) string {
 
 	numWords := [9]string{"One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"}
 
-	if pattern == 1 && leftOperand == 1 && operator == 1 {
-		return "1 + " + numWords[rightOperand-1]
+	if pattern == 1 && operator == 1 {
+		return strconv.Itoa(leftOperand) + " + " + numWords[rightOperand-1]
 	}
 
 	return ""
